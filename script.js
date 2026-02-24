3a2844c14f7935a094ca3b3be5dbe893d58a24a6
Shion Hanazawa <h01kz9tim70928@gmail.com>
2026-02-24 16:47:48 +0900

    月データ 追加

--- script.js
+++ script.js
@@ -10,13 +10,38 @@ const sampleData2 = {
     Payment: "CreditCard",
     price: 1500,
 };
+const monthList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let curMonth = new Date().getMonth() + 1;
 
 const income = sampleData["price"];
 const pay = sampleData2["price"];
 const dif = income - pay;
+
 document.querySelector(".income").textContent = income;
 document.querySelector(".pay").textContent = pay;
 document.querySelector(".dif").textContent = dif;
+document.querySelector(".header__month").textContent = `${curMonth}月`;
+
+const nextMonth = document.querySelector(".header__btn--next");
+
+nextMonth.addEventListener("click", () => {
+    let nxtMon = (curMonth += 1);
+    if (nxtMon === 13) {
+        nxtMon = 1;
+    }
+    curMonth = nxtMon;
+    document.querySelector(".header__month").textContent = `${curMonth}月`;
+});
+const prevMonth = document.querySelector(".header__btn--prev");
+
+prevMonth.addEventListener("click", () => {
+    let prvMon = (curMonth -= 1);
+    if (prvMon === 0) {
+        prvMon = 12;
+    }
+    curMonth = prvMon;
+    document.querySelector(".header__month").textContent = `${curMonth}月`;
+});
 
 const test = document.querySelector(".list--genre--li");
 const display = document.querySelector(".display");
@@ -27,14 +52,14 @@ test.addEventListener("mouseleave", function () {
     display.classList.add("hidden");
 });
 
-const buttonTest = document.querySelector(".header__btn--prev");
-buttonTest.addEventListener("click", function () {
-    alert("6月");
-});
-const buttonTest2 = document.querySelector(".header__btn--next");
-buttonTest2.addEventListener("click", function () {
-    alert("8月");
-});
+// const buttonTest = document.querySelector(".header__btn--prev");
+// buttonTest.addEventListener("click", function () {
+//     alert("6月");
+// });
+// const buttonTest2 = document.querySelector(".header__btn--next");
+// buttonTest2.addEventListener("click", function () {
+//     alert("8月");
+// });
 
 const addGenreBtn = document.querySelector(".add-genre");
 addGenreBtn.addEventListener("click", function () {
